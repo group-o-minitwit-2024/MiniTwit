27d1627ba5362edc89394efafda49715a6fc0a9b
laugekj <lakj@itu.dk>
2024-02-16 14:29:52 +0100

    Refactor director structure and remove minitwit.py and the files that are not used in Express js project

---
+++ routes/EXPRESS_ROUTE_index.js
@@ -0,0 +1,12 @@
+// This is how you do routes. But we are not using that at the moment. [LAKJ]
+
+
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
