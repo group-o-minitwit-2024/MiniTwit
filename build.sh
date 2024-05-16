5248d7e14bdc45ea98516f276281edf0304d779d
Marcus <mahf@itu.dk>
2024-05-16 12:23:14 +0200

    added options for pushing to dockerhub

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 source ./cp_shrd.sh
 
 echo "Building docker image for minitwit"
@@ -6,3 +7,20 @@ docker build -t minitwit ./app
 
 echo "Building docker image for minitwit-api"
 docker build -t minitwit-api ./API
+
+# Check if the user wants to push to Docker Hub
+if [[ "$1" == "--push" ]]; then
+    if [[ -z "$2" ]]; then
+        echo "Please provide a Docker Hub username."
+        exit 1
+    fi
+    username="$2"
+
+    echo "Pushing minitwit image to Docker Hub"
+    docker tag minitwit "$username/minitwit"
+    docker push "$username/minitwit"
+
+    echo "Pushing minitwit-api image to Docker Hub"
+    docker tag minitwit-api "$username/minitwit-api"
+    docker push "$username/minitwit-api"
+fi
