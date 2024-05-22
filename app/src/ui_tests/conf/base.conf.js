9aa4598a162bb2b30d0c1f3a13d33730b3667a98
laugekj <lakj@itu.dk>
2024-05-22 11:03:48 +0200

    added ui_tests to new file structure

---
+++ app/src/ui_tests/conf/base.conf.js
@@ -0,0 +1,31 @@
+exports.config = {
+    user: process.env.BROWSERSTACK_USERNAME,
+    key: process.env.BROWSERSTACK_ACCESS_KEY,
+  
+    updateJob: false,
+    specs: ['../specs/test.e2e.js'],
+    exclude: [],
+  
+    logLevel: 'info',
+    coloredLogs: true,
+    screenshotPath: './errorShots/',
+    baseUrl: 'http://178.62.218.96:5000',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    hostname: 'hub.browserstack.com',
+    services: [['browserstack']],
+  
+    before: function () {
+      var chai = require('chai');
+      global.expect = chai.expect;
+      chai.Should();
+    },
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+      ui: 'bdd',
+      timeout: 60000,
+    },
+  };
+  
